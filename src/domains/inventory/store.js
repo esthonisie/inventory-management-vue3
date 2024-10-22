562dafa34b43d67b5bae31bc7b9ef17bc05f4dc3
esthonisie <132005743+esthonisie@users.noreply.github.com>
2024-10-22 13:58:09 +0200

    Add inventory data to store

---
+++ src/domains/inventory/store.js
@@ -0,0 +1,14 @@
+// state
+const inventory = ref([
+  { id: 1, name: "Schetsboek A5", actualAmount: 14, minimumAmount: 10 },
+  { id: 2, name: "Vulpotlood 0.5", actualAmount: 18, minimumAmount: 15 },
+  { id: 3, name: "Potloodstiften 2B 0.5", actualAmount: 12, minimumAmount: 10 },
+  { id: 4, name: "Kneedgum", actualAmount: 14, minimumAmount: 10 },
+  { id: 5, name: "Oostindische inkt 30ml", actualAmount: 5, minimumAmount: 5 },
+  { id: 6, name: "Penhouder", actualAmount: 12, minimumAmount: 10 },
+  { id: 7, name: "Kroontjes pennetje", actualAmount: 8, minimumAmount: 15 },
+]);
+
+// getters
+
+// actions
